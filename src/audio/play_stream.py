009716b9cc171199977f4851bc609f5d004c3d20
Nelis Drost <nelis.drost@gmail.com>
2023-01-13 11:43:18 +1300

    Basic streaming audio + threading

---
+++ src/audio/play_stream.py
@@ -0,0 +1,18 @@
+from threading import Thread
+
+import sounddevice as sd
+
+
+def audio_stream(callback: callable):
+    samplerate = sd.query_devices(None, 'output')['default_samplerate']
+    stream = sd.OutputStream(device=None, channels=1,
+                             callback=callback, samplerate=samplerate/2,
+                             blocksize=2000, latency=.3)
+
+    stream.start()
+
+
+def thread_stream(callback: callable) -> Thread:
+    thread = Thread(target=audio_stream, args=[callback])
+    thread.start()
+    return thread
