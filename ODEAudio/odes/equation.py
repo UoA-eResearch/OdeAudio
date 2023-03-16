79333b5d293ee700ea623d8b9486c7502763e459
Nelis Drost <nelis.drost@gmail.com>
2023-03-17 09:50:12 +1300

    Implement 5D ODE

--- ODEAudio/odes/equation.py
+++ ODEAudio/odes/equation.py
@@ -20,3 +20,19 @@ def dy(_, y, lambda_c, lambda_e):
 def extract(t, y):
     yc = 2 * (np.exp(y[1, :]) - 0.5)
     return t, yc
+
+
+def dy5(_, y, cA, eA, cB, eB):
+
+    y2exp = np.exp(2 * y)
+
+    ytot = y2exp.sum()
+
+    vec_evals = np.asarray([-cA, eB, -cB, eA, 0])
+
+    dy = np.asarray([
+        1 - ytot + np.dot(np.roll(y2exp, -(i + 1)), vec_evals)
+        for i in range(len(y))
+    ])
+
+    return dy
