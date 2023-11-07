292ed834a01b8484ea82efd5423997529cdf51be
Nelis Drost <nelis.drost@gmail.com>
2023-11-08 11:56:10 +1300

    Pull out and document performance tuning parameters

--- ODEAudio/audio/play_stream.py
+++ ODEAudio/audio/play_stream.py
@@ -1,6 +1,16 @@
-from threading import Thread
-
 import sounddevice as sd
+
+# Performance tuning parameters
+# - Sample rate:    how many data points (per second) to play as sound
+#                   lower means lower pitch, but also less data to process
+#                   higher means higher pitch, but more performance issues
+sample_rate = 1300  # If None, uses the device default
+# - Block size:     how many data points the sound driver requests at a time
+#                   not sure if this has a performance impact
+block_size = 500
+# - Latency:        when to request new data - i.e.: how many seconds of data the driver has left when it requests more
+#                   higher values increase the input latency, but reduce instability
+latency = 0.2
 
 
 class AudioStream:
@@ -8,10 +18,11 @@ class AudioStream:
         self.callback = callback
         self.running = False
         self.samplerate = sd.query_devices(None, 'output')['default_samplerate']
-        self.samplerate = 8000
+        if sample_rate is not None:
+            self.samplerate = sample_rate
         self.stream = sd.OutputStream(device=None, channels=2,
-                                      callback=callback, samplerate=self.samplerate/6,
-                                      blocksize=1000, latency=.1)
+                                      callback=callback, samplerate=self.samplerate,
+                                      blocksize=block_size, latency=latency)
 
     def start(self):
         self.stream.start()
